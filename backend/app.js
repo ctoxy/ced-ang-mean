500c45a31df881108aec925c954f3e848c8c67a8
ctoxy <cedric.bouve@gmail.com>
2020-01-15 15:30:12 +0100

    ajout de bodyparser et cors cote backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
+/*permet d'utiliser express pour le serveur*/
 const express = require('express');
+/*permet de recuperer le contenu des requetes http via node et express*/
 const bodyParser = require("body-parser");
 /*creation de l application sous express*/
 const app = express();
 
+/*utilisation de body parser pour parser l information json issue des requetes http*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/*Cross Origin Ressource Sharing CORS to allow request between client and server on diffirent port*/
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -19,6 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
+/*REQUETE POST FOR POST*/
 app.post("/api/posts", (req, res, next) => {
   const post = req.body;
   console.log(post);
@@ -27,6 +32,7 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+/*REQUETE GET FOR POST*/
 app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
