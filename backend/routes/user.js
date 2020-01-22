50a6b45f7410a96af6514d3733b952001ca610e9
ctoxy <cedric.bouve@gmail.com>
2020-01-22 11:41:12 +0100

    ajout de bcrypt et creation d'un user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,31 @@
 /*permet d'utiliser express pour le serveur*/
 const express = require("express");
+/*permet d'utiliser bcryp pour hascher les password*/
+const bcrypt = require("bcrypt");
 
 /*using express router in the backend*/
 const router = express.Router();
 /* appel du model mongoose */
 const User = require("../models/user");
 /* route post pour le signup */
-router.post("/signup" , (req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  })
+router.post("/signup", (req, res, next) => {
+  bcrypt.hast(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user.save().then(result => {
+      res.status(201).json({
+        message: "User created",
+        result: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+  });
 });
 
-module.exports = router
+module.exports = router;
