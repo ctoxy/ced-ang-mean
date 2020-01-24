0b2567d7c23ad934c213a3a303ce0b3940a7d042
ctoxy <cedric.bouve@gmail.com>
2020-01-24 14:34:43 +0100

    creation du authguard protection des route edit et create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { PostCreateComponent } from './components/posts/post-create/post-create.
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
+import { AuthGuard } from './Guard/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: PostListComponent},
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:postId', component: PostCreateComponent ,canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
